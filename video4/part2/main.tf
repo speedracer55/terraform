5eeb22bc6bc6cb1af15c93e5e8f64053375d5ddc
speedracer55 <41237561+speedracer55@users.noreply.github.com>
2022-01-30 11:03:05 -0500

    Create main.tf

---
+++ video4/part2/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "video3-rg" {
+    location = var.location
+    name = "video3-rg-${var.env}"
+  
+}
+
+resource "azurerm_virtual_network" "video3-vnet" {
+  location = azurerm_resource_group.video3-rg.location
+  name = "video3_vnet"
+  address_space = [ "172.16.0.0/16" ]
+   resource_group_name = azurerm_resource_group.video3-rg.name
+}
+
+resource "azurerm_subnet" "video3-subnet" {
+  name                 = "video3-subnet"
+  resource_group_name  = azurerm_resource_group.video3-rg.name
+  virtual_network_name = azurerm_virtual_network.video3-vnet.name
+  address_prefixes     = ["172.16.1.0/24"]
+}
+
+resource "azurerm_network_interface" "vide03-nic" {
+    name                = "video3-nic"
+  location            = azurerm_resource_group.video3-rg.location
+  resource_group_name = azurerm_resource_group.video3-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.video3-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+  
+  resource "azurerm_windows_virtual_machine" "video3-vm" {
+    name = var.vm_name
+    admin_password = "P@$$w0rd1234!"
+    admin_username = "adminuser"
+    location = azurerm_resource_group.video3-rg.location
+    resource_group_name = azurerm_resource_group.video3-rg.name
+    size = "standard_F2"
+    network_interface_ids = [ 
+      azurerm_network_interface.vide03-nic.id,
+       ]
+   os_disk {
+     caching = "ReadWrite"
+     storage_account_type = "Standard_LRS"
+   }
+   source_image_reference {
+     offer = "WindowsServer"
+     publisher = "MicrosoftWindowsServer"
+     sku = "2019-DataCenter"
+     version = "latest"
+   }
+  }
